55179a9a2f46619f82a5ff8b163c80b0d5aa82d4
Luke Bailey <baileylu@tcd.ie>
2025-09-16 10:09:28 +0100

    Init repo

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "TEU00311 What Is The Internet Doing to Me Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+
+    blueprint = {
+      url = "github:numtide/blueprint";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.systems.follows = "systems";
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://cache.garnix.io"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+  };
+
+  outputs =
+    inputs:
+    inputs.blueprint {
+      inherit inputs;
+      prefix = "nix/";
+    };
+}
---
+++ index.md
@@ -0,0 +1,1 @@
+Notes and work for [TEU00311 What Is The Internet Doing to Me?](https://tcd.blackboard.com/ultra/courses/_102406_1/outline).
